488707ce42f29771666621f997bf9391133b8fa4
Amandine-crdn <pikouze.nurse@gmail.com>
2025-01-08 17:35:36 +0100

    p3 starteed

--- p2/scripts/master.sh
+++ p2/scripts/master.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Conf Kubernetis
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110" sh -
-kubectl apply -f /vagrant/config/web1.yaml
-kubectl apply -f /vagrant/config/ingress.yaml
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san master --node-ip 192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110" sh -
+
+kubectl apply -f /vagrant/config/web.yaml
---
+++ p3/script/config-argocd.sh
@@ -0,0 +1,17 @@
+k3d cluster create aspikrotty --api-port 6443 -p 8080:80@loadbalancer --agents 2
+kubectl config use-context k3d-aspikrotty
+
+# Creation namespace 
+kubectl create namespace argocd
+
+# Installation Argo-CD 
+kubectl apply -n argocd -f ../config/argocd-install.yaml
+kubectl apply -f ../config/argocd-ingress.yaml -n argocd
+
+# Config compte admin 
+# creds = admin/aspikrotty
+
+kubectl -n argocd patch secret argocd-secret -p '{"stringData":  {
+    "admin.password": " $2a$12$.wdRSy84NfVsjOr9D/P0UONWYKTqSNUAlXwS.10.1kpVHIp3D1c5a",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+}}'
