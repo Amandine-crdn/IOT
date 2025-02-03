2e87b738d1c48ae5cea0ac4f766f0fce3ec762e1
Amandine-crdn <pikouze.nurse@gmail.com>
2025-02-03 11:09:54 +0100

    finish

--- p3/script/setup.sh
+++ p3/script/setup.sh
@@ -14,8 +14,8 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
+#sudo usermod -aG docker $USER
+#newgrp docker
 
 ## Install kubectl
 
